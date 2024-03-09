9c63455d7a579b5d91767de892321d44a2d484e5
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-09 11:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function makeid(l) {
+function makeid(length) {
   // write your code here
+let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	console.log(characters)
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
 }
 
-// Do not change the code below.
+// Do not change the code below.  
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
